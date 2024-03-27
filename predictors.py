bda1832a24795e5d5ab8082bd85392a5413ba86d
Mirko Rupani <mirko.rupani@unican.es>
2024-03-27 09:40:04 +0100

    isomap embedding added as dimensionality reduction method

--- predictors.py
+++ predictors.py
@@ -6,6 +6,7 @@ import copernicusmarine
 import cartopy.crs as ccrs
 import numpy as np
 import os
+import pandas as pd
 
 class Predictors():
     """Class to get and load the predictors for the model"""
@@ -14,33 +15,48 @@ class Predictors():
     threddsURLMG = "https://mandeo.meteogalicia.es/thredds/dodsC/modelos/WRF_HIST/d02/"
 
 
-    def __init__(self, config):
-        
-        with open(config) as f:
+    def __init__(self, configFilePath, hisFile=None, folder="predictors"):
+
+        with open(configFilePath) as f:
             self.config = json.load(f)
         
-        self.tempExt = getTempExt(self.config["predictands"]["hisFile"])
-        self.coord = getCoord(self.config["predictands"]["hisFile"], self.config["predictands"]["station"])
-        self.windData = self.getWindData()
-        self.hydroData = self.getHydroData()
-        self.dischargeData = self.getDischargeData()
+        if hisFile is None:
+            folder = self.config["predictors"]["predictorsFolder"]
+            self.tempExt = getTempExt(self.config["predictands"]["hisFile"])
+            self.coord = getCoord(self.config["predictands"]["hisFile"], self.config["predictands"]["station"])
+            
+        else:
+            self.tempExt = getTempExt(hisFile)
+            self.coord = getCoord(hisFile, self.config["predictands"]["station"])
+    
+        self.windData = self.getWindData(folder=folder)
+        self.hydroData = self.getHydroData(folder=folder)
+        self.dischargeData = self.getDischargeData(folder=folder)
     
 
     def getWindURLs(self):
         """Gets a list of URLs to download the wind data from MeteoGalicia
         :return: list, URLs to download the wind data"""
 
+        # Expand the temporal extension one hour
+        iniDate = self.tempExt[0] - pd.Timedelta(hours=1)
+        endDate = self.tempExt[1] + pd.Timedelta(hours=1)
+
         # Get the files
         urls = []
-        for year in range(self.tempExt[0].year, self.tempExt[1].year+1):
+        for year in range(iniDate.year, endDate.year+1):
             for month in range(1, 13):
-                if year == self.tempExt[0].year and month < self.tempExt[0].month:
+                if year == iniDate.year and month < iniDate.month:
                     continue
-                if year == self.tempExt[1].year and month > self.tempExt[1].month:
+                if year == endDate.year and month > endDate.month:
                     continue
                 # Get number of days in the month
                 days = calendar.monthrange(year, month)[1]
                 for day in range(1, days+1):
+                    if year == iniDate.year and month == iniDate.month and day < iniDate.day:
+                        continue
+                    if year == endDate.year and month == endDate.month and day > endDate.day:
+                        continue
                     urls.append(f"{self.threddsURLMG}{year}/{month:02d}/wrf_arw_det_history_d02_{year}{month:02d}{day:02d}_0000.nc4")
                     if (year > 2013) or (year == 2013 and month > 1) or (year == 2013 and month == 1 and day > 27):
                         urls.append(f"{self.threddsURLMG}{year}/{month:02d}/wrf_arw_det_history_d02_{year}{month:02d}{day:02d}_1200.nc4")
@@ -48,16 +64,16 @@ class Predictors():
         return urls
 
 
-    def getWindData(self, writeNetCDF=True, overwrite=False):
+    def getWindData(self, writeNetCDF=True, overwrite=False, provider="meteogalicia", folder="predictors"):
         """Gets the wind data
         :param writeNetCDF: bool, whether to write the wind data to a netCDF file
         :return: xarray.Dataset, wind data"""
 
         # Check if file exists
-        if not overwrite and os.path.exists(os.path.join(self.config["predictors"]["predictorsFolder"],"windData.nc")):
-            return xr.open_dataset(os.path.join(self.config["predictors"]["predictorsFolder"],"windData.nc"))
+        if not overwrite and os.path.exists(os.path.join(folder,"windData.nc")):
+            return xr.open_dataset(os.path.join(folder,"windData.nc"))
 
-        if self.config["predictors"]["wind"] == "meteogalicia":
+        if provider == "meteogalicia":
 
             windUrls = self.getWindURLs()
 
@@ -86,7 +102,7 @@ class Predictors():
                 
                     # Write to netCDF
                     if writeNetCDF:
-                        ds.to_netcdf(os.path.join(self.config["predictors"]["predictorsFolder"],"windData.nc"))
+                        ds.to_netcdf(os.path.join(folder,"windData.nc"))
 
             except ValueError:
                 datasets = []
@@ -103,12 +119,12 @@ class Predictors():
                 # If there are repeated times, keep only the last one
                 ds = ds.drop_duplicates("time", keep="last")
 
-                # Trim time to the temporal extension of the model
+                # Trim time to the temporal extension
                 ds = ds.sel(time=slice(self.tempExt[0], self.tempExt[1]))
                     
                 # Write to netCDF
                 if writeNetCDF:
-                    ds.to_netcdf(os.path.join(self.config["predictors"]["predictorsFolder"],"windData.nc"))
+                    ds.to_netcdf(os.path.join(folder,"windData.nc"))
     
             return ds
         
@@ -116,14 +132,14 @@ class Predictors():
             raise ValueError("Wind data source not recognized")
     
 
-    def getHydroData(self, writeNetCDF=True, overwrite=False):
+    def getHydroData(self, writeNetCDF=True, overwrite=False, folder="predictors"):
         """Downloads the hydrodynamic data
         :param writeNetCDF: bool, whether to write the hydrodynamic data to a netCDF file
         :return: xarray.Dataset, hydrodynamic data"""
 
         # Check if file exists
-        if not overwrite and os.path.exists(os.path.join(self.config["predictors"]["predictorsFolder"], "hydroData.nc")):
-            return xr.open_dataset(os.path.join(self.config["predictors"]["predictorsFolder"], "hydroData.nc"))
+        if not overwrite and os.path.exists(os.path.join(folder, "hydroData.nc")):
+            return xr.open_dataset(os.path.join(folder, "hydroData.nc"))
 
         # Check if copernicusmarine-credentials already exists
         if not os.path.exists(os.path.join(os.path.expanduser("~"), ".copernicusmarine", ".copernicusmarine-credentials")):
@@ -162,11 +178,11 @@ class Predictors():
         
         # Write to netCDF
         if writeNetCDF:
-            ds.to_netcdf(os.path.join(self.config["predictors"]["predictorsFolder"], "hydroData.nc"))
+            ds.to_netcdf(os.path.join(folder, "hydroData.nc"))
     
         return ds
     
 
-    def getDischargeData(self, writeNetCDF=True, overwrite=False):
+    def getDischargeData(self, writeNetCDF=True, overwrite=False, folder="predictors"):
         pass
 
