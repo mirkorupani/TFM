38e14449f623ed5bc1f9eb934b5ccf6ddadc01a1
Mirko Rupani <mirko.rupani@unican.es>
2024-04-04 13:59:59 +0200

    prediction matrix name change

--- analogues.py
+++ analogues.py
@@ -13,7 +13,7 @@ class Analogues():
         
         with open(configFilePath) as f:
             self.config = json.load(f)
-        self.predictionMatrix = predictionMatrix
+        self.predMatrix = predictionMatrix
         self.clustering = self.getClustering()
         self.centroids, self.xAnalogues, self.yAnalogues = self.getAnalogues()
         self.classifier = self.getClassifier()
@@ -28,18 +28,18 @@ class Analogues():
         nClusters = self.config["model"]["analogues"]["nAnalogues"]
 
         if self.config["model"]["analogues"]["clustering"] == "kMeans":
-            return KMeans(n_clusters=nClusters, random_state=randomState).fit(self.predictionMatrix.xTrain)
+            return KMeans(n_clusters=nClusters, random_state=randomState).fit(self.predMatrix.xTrain)
         
         elif self.config["model"]["analogues"]["clustering"] == "maxDiss":
-            if isinstance(self.predictionMatrix.xTrain, pd.DataFrame):
-                dataTuple = tuple([self.predictionMatrix.xTrain[col] for col in self.predictionMatrix.xTrain.columns])
-            elif isinstance(self.predictionMatrix.xTrain, np.ndarray):
-                dataTuple = tuple([self.predictionMatrix.xTrain[:, i] for i in range(self.predictionMatrix.xTrain.shape[1])])
+            if isinstance(self.predMatrix.xTrain, pd.DataFrame):
+                dataTuple = tuple([self.predMatrix.xTrain[col] for col in self.predMatrix.xTrain.columns])
+            elif isinstance(self.predMatrix.xTrain, np.ndarray):
+                dataTuple = tuple([self.predMatrix.xTrain[:, i] for i in range(self.predMatrix.xTrain.shape[1])])
             return max_diss_alg(dataTuple, nClusters, seed_index=randomState)
         
         elif self.config["model"]["analogues"]["clustering"] == "spectral":
             spectralClustering = SpectralClustering(n_clusters=nClusters, random_state=randomState)
-            return spectralClustering.fit(self.predictionMatrix.xTrain)
+            return spectralClustering.fit(self.predMatrix.xTrain)
     
 
     def getAnalogues(self):
@@ -47,7 +47,7 @@ class Analogues():
 
         # Get the analogues
         if self.config["model"]["analogues"]["clustering"] == "kMeans":
-            kNN1 = KNeighborsClassifier(n_neighbors=1).fit(self.predictionMatrix.xTrain, range(len(self.predictionMatrix.xTrain)))
+            kNN1 = KNeighborsClassifier(n_neighbors=1).fit(self.predMatrix.xTrain, range(len(self.predMatrix.xTrain)))
             centroids = kNN1.predict(self.clustering.cluster_centers_)
         elif self.config["model"]["analogues"]["clustering"] == "maxDiss":
             centroids = self.clustering[1]
@@ -55,11 +55,11 @@ class Analogues():
             centroids = np.empty(self.clustering.n_clusters, dtype=int)
             for i in range(self.clustering.n_clusters):
                 clusterIndices = np.where(self.clustering.labels_ == i)[0]
-                clusterDataPoints = self.predictionMatrix.xTrain[clusterIndices]
+                clusterDataPoints = self.predMatrix.xTrain[clusterIndices]
                 distances = np.sum((clusterDataPoints[:, np.newaxis] - clusterDataPoints)**2, axis=-1)
                 centroids[i] = clusterIndices[np.argmin(np.sum(distances, axis=1))]
 
-        return centroids, self.predictionMatrix.xTrain[centroids], self.predictionMatrix.yTrain.iloc[centroids]
+        return centroids, self.predMatrix.xTrain[centroids], self.predMatrix.yTrain.iloc[centroids]
     
 
     def getClassifier(self):
@@ -73,7 +73,7 @@ class Analogues():
         elif self.config["model"]["analogues"]["clustering"] == "maxDiss":
             return classifier.fit(self.clustering[0], self.centroids)
         elif self.config["model"]["analogues"]["clustering"] == "spectral":
-            return classifier.fit(self.predictionMatrix.xTrain, self.clustering.labels_)
+            return classifier.fit(self.predMatrix.xTrain, self.clustering.labels_)
         
 
     def predict(self, x):
